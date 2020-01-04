56566611aa4d78a5b9e2254754bd5d4b7fa17647
Skoczek1992 <s.skoczylas92@gmail.com>
2020-01-04 12:06:19 +0100

    add hbs and layout

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
 
 app.use(express.static(path.join(__dirname + '/public')));
 
@@ -16,37 +14,48 @@ app.use('/user', (req, res, next) => {
   res.send('Please log in');
   next();
 });
-
 app.get('/user/panel', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/forbidden.html'));
 });
-
 app.get('/user/settings', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/forbidden.html'));
 });
 
 
-app.get('/hello/:name', (req, res) => {
-  res.send(`Hello ${req.params.name}`);
-});
-
-
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('home');
 });
 
 app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
+
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname + `/views/404.html`));
-})
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
-}); 
+});
